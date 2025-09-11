5d7f5a342de8988d3a85ca5b2004553985532523
in-du1695 <indukurumutla@email.com>
2025-09-11 15:09:26 +0530

    Soc

---
+++ Day3/new11.js
@@ -0,0 +1,120 @@
+// console.log("Hello")
+
+// no=16;
+// str='bye';
+// bol=true;
+// BigInt =29742983649372;
+
+// undefined;
+// null=null;
+ age=11;
+// console.log(age)
+// const givename='indu';
+// console.log(givename)
+
+// for(i=0;i<5;i++)
+//     {
+//         console.log(i)
+//     }
+
+//     for(i=1;i<=10;i++)
+//     {
+//         j=2;
+//         console.log(j+'*'+i+'='+j*i)
+//     }
+
+//function
+
+// function generatemessage(name)
+// {
+//     return 'Hello '+name +' welcome to the world of javascript';
+// }
+
+// let welcome=generatemessage('Indu');
+// console.log(welcome);
+
+// // caluculator
+
+// function add(a,b)
+// {
+//     return a+b;
+// }
+// function sub(a,b)
+// {
+//     return a-b;
+// }
+// function mul(a,b)
+// {
+//     return a*b;
+// }
+// function div(a,b)
+// {
+//     if(b==0)
+//     {
+//         return 'Division by zero is not allowed';
+//     }
+//     return a/b;
+// }
+// // let addition=add(9,16)
+// // let subtraction=sub(255,16);
+// // let multiplication=mul(5,6);
+// // let division=div(25,5);
+// console.log('addition',add(9,16));
+// console.log('subtraction',sub(255,16));
+// console.log('multiplication',mul(5,6));
+// console.log('division',div(25,0));
+
+
+//operations
+
+// sc = 90;
+// let addsc=sc+20;
+// let subsc=sc-90;
+// let mulsc=sc*3;
+// let divsc=sc/2;
+
+// console.log(addsc);
+// console.log(subsc);
+// console.log(mulsc);
+// console.log(divsc);
+
+//area of circle
+
+// let radius=5;
+// let area=3.14*radius*radius;
+// console.log(area);
+//     fact=1;
+
+//factorial of a number
+
+// for(i=5;i>=1;i--)
+// {
+//     fact=fact*i;
+    
+//    }
+//     console.log(fact);
+
+//     fact1=1;
+//     function gen(no)
+// {
+//     for(i=no;i>=1;i--)
+// {
+//     fact1=fact1*i;
+// }
+//     return fact1;
+// }
+
+// let res=gen(5);
+// console.log(res);
+
+
+//compound interest
+let p=1000;
+let r=10;
+let t=2;
+let ci=p*(Math.pow((1+r/100),t));
+console.log(ci);
+
+let si=(p*r*t)/100;  //simple interest
+console.log(si);
+
